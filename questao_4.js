46dd4ec7943e2c9dc6f39da129638fc6ccdc6090
Vinicius 'Davina' Ferreira <63323842+davinnye@users.noreply.github.com>
2024-09-23 20:18:47 -0300

    questão 4 solucionada

---
+++ questao_4.js
@@ -0,0 +1,18 @@
+let sp = 67836.43;
+let rj = 36678.66;
+let mg = 29229.88;
+let es = 27165.48;
+let others = 19849.53;
+let totalAmount = sp + rj + mg + es + others;
+
+let spPercentage = (sp * 100) / totalAmount;
+let rjPercentage = (rj * 100) / totalAmount;
+let mgPercentage = (mg * 100) / totalAmount;
+let esPercentage = (es * 100) / totalAmount;
+let othersPercentage = (others * 100) / totalAmount;
+
+console.log(spPercentage);
+console.log(rjPercentage);
+console.log(mgPercentage);
+console.log(esPercentage);
+console.log(othersPercentage);
