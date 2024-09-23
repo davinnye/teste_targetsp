6913c6f6b94aadad204ea00a84bb8d9291f99a49
Vinicius 'Davina' Ferreira <63323842+davinnye@users.noreply.github.com>
2024-09-23 19:04:54 -0300

    questão 1 solucionada

---
+++ questao_1.java
@@ -0,0 +1,13 @@
+public class questao_1 {
+
+    public static void main(String[] args) {
+        int indice = 13;
+        long soma = 0;
+
+        for (int k = 1; k < indice; k++) {
+            soma = soma + k;
+        }
+        System.out.println("O valor final da variável SOMA será de: " + soma);
+        //o valor total é de 78
+    }
+}
